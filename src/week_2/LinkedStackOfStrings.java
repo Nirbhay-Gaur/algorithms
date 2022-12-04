50d4021ce48c79a6d11f6fb08b3dabedb949eb7c
Nirbhay-Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2022-12-04 23:04:54 +0530

    add stack implementation using linked list

---
+++ src/week_2/LinkedStackOfStrings.java
@@ -0,0 +1,49 @@
+package week_2;
+
+public class LinkedStackOfStrings {
+
+    private Node first = null;
+    private int size = 0;
+
+    public static void main(String[] args) {
+        LinkedStackOfStrings stack = new LinkedStackOfStrings();
+
+        stack.push("abc");
+        stack.push("def");
+        stack.push("ghi");
+        stack.push("jkl");
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop() + " " + stack.getSize());
+        }
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(String x) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = x;
+        first.next = oldFirst;
+        size++;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public String pop() {
+        Node temp = first;
+        first = first.next;
+        size--;
+        return temp.item;
+    }
+
+    private static class Node {
+        String item;
+        Node next;
+    }
+
+}
