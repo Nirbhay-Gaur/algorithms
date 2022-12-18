3cab5059a113f87f8ce30f601cced0dda24c868e
Nirbhay-Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2022-12-18 13:50:47 +0530

    add shuffling linked list solution

--- src/week_3/assignments/ShuffleLinkedList.java
+++ src/week_3/assignments/ShuffleLinkedList.java
@@ -8,8 +8,51 @@ public class ShuffleLinkedList {
         ShuffleLinkedList shuffleLinkedList = new ShuffleLinkedList();
         shuffleLinkedList.createList(10);
         shuffleLinkedList.print();
-        shuffleLinkedList.shuffle(shuffleLinkedList.head, shuffleLinkedList.tail);
+        shuffleLinkedList.shuffle();
         shuffleLinkedList.print();
+    }
+
+    public void shuffle() {
+        this.head = sort(this.head);
+    }
+
+    private Node<Integer> sort(Node<Integer> _head) {
+        if (_head == null || _head.next == null) return _head;
+        Node<Integer> slow = _head;
+        Node<Integer> fast = _head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node<Integer> left = _head;
+        Node<Integer> right = slow.next;
+        slow.next = null;
+
+        left = sort(left);
+        right = sort(right);
+        return merge(left, right);
+    }
+
+    private Node<Integer> merge(Node<Integer> _left, Node<Integer> _right) {
+        Node<Integer> aux = new Node<>();
+        Node<Integer> l = _left;
+        Node<Integer> r = _right;
+        Node<Integer> current = aux;
+        while (l != null && r != null) {
+            int random = Math.random() > 0.5 ? 1 : 0;
+            if (random == 0) {
+                current.next = l;
+                current = current.next;
+                l = l.next;
+            } else {
+                current.next = r;
+                current = current.next;
+                r = r.next;
+            }
+        }
+        if (l != null) current.next = l;
+        else if (r != null) current.next = r;
+        return aux.next;
     }
 
     public void print() {
@@ -23,23 +66,6 @@ public class ShuffleLinkedList {
         System.out.println(" ");
     }
 
-    public void shuffle(Node<Integer> _head, Node<Integer> _tail) {
-
-        Node<Integer> middle = findMiddle(_head);
-        if (middle == _tail) return;
-        shuffle(_head, middle);
-        shuffle(middle.next, _tail);
-        swap(_head, middle, _tail);
-    }
-
-    private void swap(Node<Integer> _head, Node<Integer> _middle, Node<Integer> _tail) {
-        while (_head.next != null && _head.next.next != null) {
-            int value = _head.item;
-            _head.item = _head.next.item;
-            _head.next.item = value;
-            _head = _head.next.next;
-        }
-    }
 
     private Node<Integer> findMiddle(Node<Integer> head) {
         Node<Integer> middle = head;
