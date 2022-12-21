0e122f5aaa310b81ae7b91a9460ea806a979c8e9
Nirbhay-Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2022-12-21 16:07:48 +0530

    add nuts and bolts algorithm

--- src/week_3/NutsAndBolts.java
+++ src/week_3/NutsAndBolts.java
@@ -2,47 +2,50 @@ package week_3;
 
 public class NutsAndBolts {
     public static void main(String[] args) {
-        int[] nuts = {2, 1, 4, 3, 5};
-        int[] bolts = {5, 3, 4, 2, 1};
+        int[] nuts = new int[]{3, 4, 1, 2, 5};
+        int[] bolts = new int[]{4, 2, 1, 3, 5};
 
-        solve(nuts, bolts, 0, 4);
+        solve(nuts, bolts, 0, nuts.length - 1);
 
-        print(nuts);
-        print(bolts);
-    }
-
-    private static void print(int[] a) {
-        for (int x : a) System.out.print(x + " ");
-        System.out.println(" ");
-    }
-
-    private static int partition(int[] array, int low, int high, int pivot) {
-        int i = low;
-
-        for (int j = low; j < high; j++) {
-            if (array[j] < pivot) {
-                swap(array, i, j);
-                i++;
-            } else if (array[j] == pivot) {
-                swap(array, j, high);
-                j--;
-            }
-        }
-        swap(array, i, high);
-        return i;
-    }
-
-    private static void swap(int[] a, int x, int y) {
-        int temp = a[x];
-        a[x] = a[y];
-        a[y] = temp;
+        for (int nut : nuts) System.out.print(nut + " ");
+        System.out.println(' ');
+        for (int bolt : bolts) System.out.print(bolt + " ");
     }
 
     public static void solve(int[] nuts, int[] bolts, int low, int high) {
-        if (high <= low) return;
-        int pivot = partition(nuts, low, high, bolts[high]);
-        partition(bolts, low, high, nuts[pivot]);
-        solve(nuts, bolts, low, pivot - 1);
-        solve(nuts, bolts, pivot + 1, high);
+        if (low >= high) return;
+        int idx = partition(nuts, low, high, bolts[high]);
+        partition(bolts, low, high, nuts[idx]);
+        solve(nuts, bolts, low, idx - 1);
+        solve(nuts, bolts, idx + 1, high);
     }
+
+    private static int partition(int[] a, int low, int high, int pivot) {
+        int i = low;
+        int j = high;
+        while (i <= j) {
+            if (a[i] > pivot) {
+                swap(a, i, j);
+                j--;
+            } else if (a[j] < pivot) {
+                swap(a, i, j);
+                i++;
+            } else if (a[i] == pivot) {
+                swap(a, i, low);
+                i++;
+            } else if (a[j] == pivot) {
+                swap(a, j, low);
+                i++;
+            }
+        }
+        swap(a, low, j);
+        return j;
+    }
+
+    public static void swap(int[] a, int x, int y) {
+        int t = a[x];
+        a[x] = a[y];
+        a[y] = t;
+    }
+
 }
