eab450f63954a6986bef852f6e295995d2c5675c
Nirbhay-Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2022-12-23 00:27:43 +0530

    add binary heap implementation

--- src/week_4/BinaryHeap.java
+++ src/week_4/BinaryHeap.java
@@ -1,44 +1,28 @@
 package week_4;
 
 public class BinaryHeap {
-
-    private int[] array;
-    private int size = 0;
-
+    private final int[] array;
+    private int size;
 
     public BinaryHeap(int capacity) {
         array = new int[capacity];
+        size = 0;
     }
 
     public static void main(String[] args) {
-        int[] a = new int[]{4, 2, 3, 1, 5, 6, 8, 7, 9};
+        int[] a = new int[]{2, 3, 1, 5, 6, 7, 8, 9, 4};
         BinaryHeap binaryHeap = new BinaryHeap(a.length + 1);
         for (int x : a) binaryHeap.insert(x);
+        while (!binaryHeap.isEmpty()) System.out.print(binaryHeap.deleteMax() + " ");
+    }
 
-        while (!binaryHeap.isEmpty()) {
-            System.out.print(binaryHeap.deleteMax() + " ");
-        }
+    public void print() {
+        for (int x : array) System.out.print(x + " ");
+        System.out.println(" ");
     }
 
     public boolean isEmpty() {
         return size == 0;
-    }
-
-    private void swim(int k) {
-        while (k > 1 & array[k / 2] < array[k]) {
-            swap(k, k / 2);
-            k = k / 2;
-        }
-    }
-
-    private void sink(int k) {
-        while (2 * k <= size) {
-            int j = 2 * k;
-            if (j < size && array[j] < array[j + 1]) j++;
-            if (array[j] < array[k]) break;
-            swap(j, k);
-            k = j;
-        }
     }
 
     public void insert(int x) {
@@ -53,10 +37,26 @@ public class BinaryHeap {
         return max;
     }
 
+    private void swim(int k) {
+        while (k > 1 && array[k] > array[k / 2]) {
+            swap(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k < size) {
+            int j = 2 * k;
+            if (j < size && array[j] < array[j + 1]) j++;
+            if (array[k] > array[j]) break;
+            swap(j, k);
+            k = j;
+        }
+    }
+
     private void swap(int x, int y) {
         int temp = array[x];
         array[x] = array[y];
         array[y] = temp;
     }
-
 }
