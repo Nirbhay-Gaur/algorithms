26bae664696698c9bca6ce124d73cecf3b2a0267
Nirbhay-Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2022-12-22 14:48:44 +0530

    add square root solution

---
+++ src/week_4/SquareRoot.java
@@ -0,0 +1,24 @@
+package week_4;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        System.out.println(sqrt(81));
+        System.out.println(sqrt(256));
+        System.out.println(sqrt(1024));
+        System.out.println(sqrt(99999));
+
+    }
+
+    public static int sqrt(int x) {
+        if (x == 1) return 1;
+        int l = 0;
+        int r = x;
+        while (l < r - 1) {
+            int mid = l + (r - l) / 2;
+            if (mid == x / mid) return mid;
+            else if (mid < x / mid) l = mid;
+            else if (mid > x / mid) r = mid;
+        }
+        return l;
+    }
+}
