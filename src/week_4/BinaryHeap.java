d5227cb27c0c50efc8d303f8f5621e3af9703b75
nirbhay-gaur <nirbhaygaur050@gmail.com>
2023-01-02 00:24:04 +0530

    add binary heap algorithm

--- src/week_4/BinaryHeap.java
+++ src/week_4/BinaryHeap.java
@@ -1,40 +1,28 @@
 package week_4;
 
 public class BinaryHeap {
-    private final int[] array;
-    private int size;
-
-    public BinaryHeap(int capacity) {
-        array = new int[capacity];
-        size = 0;
-    }
-
     public static void main(String[] args) {
-        int[] a = new int[]{2, 3, 1, 5, 6, 7, 8, 9, 4};
+        char[] a = new char[]{'c', 's', 'n', 'a', 'b'};
         BinaryHeap binaryHeap = new BinaryHeap(a.length + 1);
-        for (int x : a) binaryHeap.insert(x);
+        for (char c : a) binaryHeap.insert(c);
         while (!binaryHeap.isEmpty()) System.out.print(binaryHeap.deleteMax() + " ");
     }
 
-    public void print() {
-        for (int x : array) System.out.print(x + " ");
-        System.out.println(" ");
+    private final char[] array;
+    private int N;
+
+    public BinaryHeap(int capacity) {
+        array = new char[capacity];
+        N = 0;
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return N == 0;
     }
 
-    public void insert(int x) {
-        array[++size] = x;
-        swim(size);
-    }
-
-    public int deleteMax() {
-        int max = array[1];
-        swap(1, size--);
-        sink(1);
-        return max;
+    public void insert(char x) {
+        array[++N] = x;
+        swim(N);
     }
 
     private void swim(int k) {
@@ -44,19 +32,28 @@ public class BinaryHeap {
         }
     }
 
-    private void sink(int k) {
-        while (2 * k < size) {
-            int j = 2 * k;
-            if (j < size && array[j] < array[j + 1]) j++;
-            if (array[k] > array[j]) break;
-            swap(j, k);
-            k = j;
-        }
-    }
-
     private void swap(int x, int y) {
-        int temp = array[x];
+        char temp = array[x];
         array[x] = array[y];
         array[y] = temp;
+    }
+
+    public char deleteMax() {
+        char max = array[1];
+        swap(1, N);
+        N--;
+        sink(1);
+        array[N + 1] = '\0';
+        return max;
+    }
+
+    private void sink(int k) {
+        while (2 * k < N) {
+            int j = 2 * k;
+            if (j < N && array[j + 1] > array[j]) j++;
+            if (array[j] < array[k]) break;
+            swap(k, j);
+            k = j;
+        }
     }
 }
